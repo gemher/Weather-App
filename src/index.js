0a510c3bc3a50ca0d3ec16313bf12c9c2ff699cb
gemher <gema.334@gmail.com>
2022-03-02 01:12:13 -0600

    Added weather attribute HTML

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ h3.innerHTML = `${day} ${hour}:${min}`;
 function search(event) {
   event.preventDefault();
   let cityName = document.querySelector("#city-input");
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = cityName.value;
+  let city = document.querySelector("city");
+  city.innerHTML = cityName.value;
   let apiKey = "021be1bf6bbbf0a54cf5f03d5e6f32ee";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
